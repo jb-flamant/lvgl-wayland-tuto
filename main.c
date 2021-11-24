a1c2d858c11cfa58caa09226b02cb88b3bfa2628
Jean-Baptiste FLAMANT <jean-baptiste.flamant@smile.fr>
2021-11-24 14:37:23 +0100

    First wayland example - empty

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #define _DEFAULT_SOURCE /* needed for usleep() */
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include "lvgl/lvgl.h"
 #include "lv_drivers/display/monitor.h"
@@ -14,27 +15,28 @@
 
 #include "lv_drivers/wayland/wayland.h"
 
-#include "lvgl/examples/lv_examples.h"
-
 /**********************
  *  STATIC PROTOTYPES
  **********************/
 static void hal_init(void);
-static int tick_thread(void *data);
+static pthread_t thread1;
+static void* tick_thread(void* data);
 
 int main(int argc, char **argv)
 {
-  (void)argc; /*Unused*/
-  (void)argv; /*Unused*/
+    (void)argc; /*Unused*/
+    (void)argv; /*Unused*/
 
     /*Initialize LVGL*/
     lv_init();
-    wayland_init();
+    lv_wayland_init();
 
     /*Initialize the HAL (display, input devices, tick) for LVGL*/
     hal_init();
 
-    lv_example_get_started_1();
+    pthread_create( &thread1, NULL, tick_thread,NULL);
+
+//    lv_example_get_started_1();
 
     while(1) {
         /* Periodically call the lv_task handler.
@@ -43,7 +45,8 @@ int main(int argc, char **argv)
         usleep(5 * 1000);
     }
 
-    wayland_deinit();
+    pthread_join(thread1,NULL);
+    lv_wayland_deinit();
     lv_deinit();
 
     return 0;
@@ -60,47 +63,11 @@ int main(int argc, char **argv)
 static void hal_init(void)
 {
 
-/* Add a display */
-  static lv_disp_draw_buf_t draw_buf;
-  static lv_color_t buf1[WAYLAND_HOR_RES * WAYLAND_VER_RES];
-  lv_disp_draw_buf_init(&draw_buf, buf1, NULL, WAYLAND_HOR_RES * WAYLAND_VER_RES);
-
-  /* Create a display */
-  static lv_disp_drv_t disp_drv;
-  lv_disp_drv_init(&disp_drv);
-  disp_drv.draw_buf = &draw_buf;
-  disp_drv.hor_res = WAYLAND_HOR_RES;
-  disp_drv.ver_res = WAYLAND_VER_RES;
-  disp_drv.flush_cb = wayland_flush;
-  lv_disp_t * disp = lv_disp_drv_register(&disp_drv);
-
-  /*Add a keyboard*/
-  lv_indev_drv_t indev_drv_kb;
-  lv_indev_drv_init(&indev_drv_kb);
-  indev_drv_kb.type = LV_INDEV_TYPE_KEYPAD;
-  indev_drv_kb.read_cb = wayland_keyboard_read;
-  lv_indev_drv_register(&indev_drv_kb);
-
-//  Add touchscreen:
-    lv_indev_drv_t indev_drv_touch;
-    lv_indev_drv_init(&indev_drv_touch);
-    indev_drv_touch.type = LV_INDEV_TYPE_POINTER;
-    indev_drv_touch.read_cb = wayland_touch_read;
-    lv_indev_drv_register(&indev_drv_touch);
-
-//  Add mouse:
-    lv_indev_drv_t indev_drv_mouse;
-    lv_indev_drv_init(&indev_drv_mouse);
-    indev_drv_mouse.type = LV_INDEV_TYPE_POINTER;
-    indev_drv_mouse.read_cb = wayland_pointer_read;
-    lv_indev_drv_register(&indev_drv_mouse);
-
-//  Add mouse wheel as encoder:
-    lv_indev_drv_t indev_drv_mousewheel;
-    lv_indev_drv_init(&indev_drv_mousewheel);
-    indev_drv_mousewheel.type = LV_INDEV_TYPE_ENCODER;
-    indev_drv_mousewheel.read_cb = wayland_pointeraxis_read;
-    lv_indev_drv_register(&indev_drv_mousewheel);
+    /* Create a display */
+    lv_disp_t * disp = lv_wayland_create_window(WAYLAND_HOR_RES,
+                                                WAYLAND_VER_RES,
+                                                WAYLAND_SURF_TITLE,
+                                                NULL);
 
 }
 
@@ -109,12 +76,10 @@ static void hal_init(void)
  * @param data unused
  * @return never return
  */
-static int tick_thread(void *data) {
-    (void)data;
-
+static void *tick_thread(void* data) {
+    (void) data;
     while(1) {
+        usleep(5 * 1000);
         lv_tick_inc(5); /*Tell LittelvGL that 5 milliseconds were elapsed*/
     }
-
-    return 0;
 }
